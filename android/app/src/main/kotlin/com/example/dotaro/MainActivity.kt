882523424938ae277ac3c8b24a88d9a4542a7d16
Erigers <erigersmamoci@gmail.com>
2021-01-09 15:54:03 +0100

    homepage app bar and home background

---
+++ android/app/src/main/kotlin/com/example/dotaro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dotaro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/settings/Palette.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static const Color background = Color(0xFF010400);
+  static const Color surface = Color(0xFF001118);
+  static const Color primary = Color(0xFFAE1B1D);
+  static const Color secondary = Color(0xFF05A670);
+  static const Color onBackground = Color(0xFFFFFFFF);
+  static const Color onSurface = Color(0xFFFFFFFF);
+  static const Color onPrimary = Color(0xFFFFFFFF);
+  static const Color onSecondary = Color(0xFF001118);
+}
